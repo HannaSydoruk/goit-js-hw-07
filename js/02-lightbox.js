ca8a51fd0025a8a609606cded018a8e4f90af2b8
HannaSydoruk <hanna.sydoruk@gmail.com>
2023-10-10 15:22:15 +0300

    Task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+function createImageGallery(galleryItems) {
+    const galleryUl = document.querySelector('.gallery');
+
+    const markup = galleryItems.map(({ preview, original, description }) => {
+        return `
+        <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+        />
+    </a>
+</li>`
+    }).join('');
+
+    galleryUl.innerHTML = markup;
+}
+
+createImageGallery(galleryItems);
+
+var lightbox = new SimpleLightbox('.gallery > li > a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+    captionPosition: 'bottom',
+});
